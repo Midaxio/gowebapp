e02a7af58707f5981fd7deb331764397411d53da
Unai Ruiz Tens <unai@midax-consulting.co.uk>
2015-11-13 10:46:19 +0000

    Added basic nginx proxy cookbook

---
+++ workspace/chef/gowebapp/cookbooks/nginx_proxy/recipes/default.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: nginx_proxy
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'apt'
+include_recipe "nginx"
+
+template '/etc/nginx/conf.d/upstreams.conf' do
+    source 'upstreams.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0640'
+end
+
+template '/etc/nginx/sites-available/gosite' do
+    source 'gosite.erb'
+    owner 'root'
+    group 'root'
+    mode '0640'
+end
+
+nginx_site 'gosite' do
+    enable true
+end
