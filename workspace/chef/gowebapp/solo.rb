d620076a05225146991385eb2597df6cb4d3dba1
Unai Ruiz Tens <unai@midax-consulting.co.uk>
2015-11-13 10:47:25 +0000

    Added chef-solo required config

---
+++ workspace/chef/gowebapp/solo.rb
@@ -0,0 +1,6 @@
+file_cache_path "/tmp/chef"
+cookbook_path [
+    "/home/ubuntu/workspace/chef/gowebapp/cookbooks",
+    "/home/ubuntu/workspace/chef/gowebapp/vendor-cookbooks"
+    ]
+json_attribs "/home/ubuntu/workspace/chef/gowebapp/node.json"
