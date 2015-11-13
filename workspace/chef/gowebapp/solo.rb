b464b244af52ee075196b2a411b85bf960e5a9cb
Unai Ruiz Tens <unai@midax-consulting.co.uk>
2015-11-13 15:48:23 +0000

    Removed attribs from solo.rb as it is specified from the command line

--- workspace/chef/gowebapp/solo.rb
+++ workspace/chef/gowebapp/solo.rb
@@ -3,4 +3,3 @@ cookbook_path [
     "/home/ubuntu/repo/workspace/chef/gowebapp/cookbooks",
     "/home/ubuntu/repo/workspace/chef/gowebapp/vendor-cookbooks"
     ]
-json_attribs "/home/ubuntu/repo/workspace/chef/gowebapp/node.json"
