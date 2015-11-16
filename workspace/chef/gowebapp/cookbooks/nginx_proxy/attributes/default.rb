526cb5716e0046f19de7419acb46af984712f02c
Unai Ruiz Tens <unai@midax-consulting.co.uk>
2015-11-16 00:12:16 +0000

    We now have proper dns entries rather than ugly ips

--- workspace/chef/gowebapp/cookbooks/nginx_proxy/attributes/default.rb
+++ workspace/chef/gowebapp/cookbooks/nginx_proxy/attributes/default.rb
@@ -1,2 +1,2 @@
 default['nginx']['default_site_enabled'] = false
-default['nginx_proxy']['appservers'] = ['172.31.46.69:8484', '172.31.46.68:8484']
+default['nginx_proxy']['appservers'] = ['app1.dev.midax.io:8484', 'app2.dev.midax.io:8484']
