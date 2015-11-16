2f59128d56095f2d548e81da2650458205d5f2cc
Unai Ruiz Tens <unai@midax-consulting.co.uk>
2015-11-16 00:26:46 +0000

    update apt-cache

--- devbox/scripts/install.sh
+++ devbox/scripts/install.sh
@@ -9,6 +9,7 @@ dpkg -i /tmp/files/chefdk.deb
 mv /tmp/files/id_rsa /home/${DEV_USER}/.ssh/
 chown $DEV_USER /home/${DEV_USER}/.ssh/id_rsa
 chmod 0600 /home/${DEV_USER}/.ssh/id_rsa
+apt-get update
 apt-get -y install git python-pip
 pip install ansible
 pip install awscli
