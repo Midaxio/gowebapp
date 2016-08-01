c0db99944949f789e012e9407880ac7846f1c447
Unai Ruiz Tens <unai@midax.io>
2016-08-01 01:02:02 +0100

    force ansible version

--- devbox/scripts/install.sh
+++ devbox/scripts/install.sh
@@ -10,7 +10,7 @@ mv /tmp/files/id_rsa /home/${DEV_USER}/.ssh/
 chown $DEV_USER /home/${DEV_USER}/.ssh/id_rsa
 chmod 0600 /home/${DEV_USER}/.ssh/id_rsa
 apt-get update
-apt-get -y install git python-pip
-pip install ansible
+apt-get -y install git python-pip python-dev libffi-dev libssl-dev build-essential
+pip install ansible==1.9.4
 pip install awscli
 pip install boto
